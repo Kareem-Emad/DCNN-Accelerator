e75bdd3b3c2918d7d34b7fd491104c9cf7371fc6
rka <akregeb@gmail.com>
2019-04-11 14:42:52 +0200

    Adding a test bench for the register.

--- src/common/register.vhd
+++ src/common/register.vhd
@@ -1,21 +1,22 @@
 library ieee;
 use ieee.std_logic_1164.all;
+library work;
 
-entity Register is
-    generic(
+entity reg is
+    generic (
         N       : natural := 16 -- number of bits
     ); 
-    port(
+    port (
         d_in    : in std_logic_vector(N - 1 downto 0); -- parallel input
         load_in : in std_logic; -- load in data.
         q_out   : out std_logic_vector(N - 1 downto 0) := (others => '0'); -- parallel output
         clk     : in std_logic; -- clock
         reset   : in std_logic -- reset
     );
-end Register;
+end reg;
 
 
-architecture Behavioral of Register is
+architecture Behavioral of reg is
 begin
     process(clk, reset)
     begin
---
+++ tests/common/reg_tb.vhd
@@ -0,0 +1,49 @@
+library ieee;
+use ieee.std_logic_1164.all;
+library work;
+
+entity RegTB is
+end RegTB;
+
+architecture TB of RegTB is
+    signal clk, load, reset : std_logic := '0';
+    signal data_in, data_out : std_logic_vector(15 downto 0) := (others => '1');
+    constant period : time := 1 ns;
+
+begin
+    reg_inst : entity work.Reg
+        generic map (
+            N => 16
+        )
+        port map (
+            clk => clk,
+            reset => reset,
+            load_in => load,
+            d_in => data_in,
+            q_out => data_out 
+        );
+
+    process is
+    begin
+        load <= '1';
+        data_in <= X"1234";
+        wait for period;
+        load <= '0';
+        assert (data_out = X"1234") report "Writing to register failed!";
+        wait for period;
+        assert (data_out = X"1234") report "Register does not latch!";
+        wait for period;
+        reset <= '1';
+        wait for period;
+        assert (data_out = X"0000") report "Reset does not work!";
+        reset <= '0';
+    end process;
+
+    process is
+    begin
+        clk <= '0';
+        wait for period / 2;
+        clk <= '1';
+        wait for period / 2;
+    end process;
+end TB;
