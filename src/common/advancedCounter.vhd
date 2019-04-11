aaf48a04e14235e0e604eae7ec64c9e2c1257203
rka <akregeb@gmail.com>
2019-04-11 18:00:57 +0200

    Advanced Counter synthesizes

--- src/common/advancedCounter.vhd
+++ src/common/advancedCounter.vhd
@@ -4,7 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity AdvancedCounter is -- NOTE: COUNTER VALUE SHOULD BE RESET ON THE OUTSIDE FOR COMPUTATIONAL REASONS
     generic (
-        N : natural := 16 --to be set otherwise
+        N : natural := 5 --to be set otherwise
     );
 
     port (
@@ -22,25 +22,27 @@ architecture behavioural of AdvancedCounter is
     signal counter_data : unsigned(N-1 downto 0);
     signal counter_data_v : std_logic_vector (N-1 downto 0);
 begin
-    process(reset, clk)
+    process(reset, enable, clk)
     begin
         if reset ='1' then
             counter_data <= (others => '0');
-        elsif rising_edge(clk) and enable = '1' then
-            if mode_in = "00" then
-                counter_data <= counter_data + 1;
-            elsif mode_in = "01" then
-                counter_data <= counter_data - 1;
-            elsif mode_in = "10" then
-                counter_data <= counter_data + 2;
-            else
-                counter_data <= counter_data - 2;
+        elsif rising_edge(clk) then
+            if enable = '1' then
+                if mode_in = "00" then
+                    counter_data <= counter_data + 1;
+                elsif mode_in = "01" then
+                    counter_data <= counter_data - 1;
+                elsif mode_in = "10" then
+                    counter_data <= counter_data + 2;
+                else
+                    counter_data <= counter_data - 2;
+                end if;
             end if;
         end if;
     end process;
 
     counter_data_v <= std_logic_vector(counter_data);
-    counter_out <= counter_data_v;
+    counter_out <= counter_data_v when enable = '1' else (others => '0');
     max_reached_out <= '1' when counter_data_v = max_val_in else '0';
 end behavioural;
   
---
+++ syn/common/counter/advanced_counter.tcl
@@ -0,0 +1,14 @@
+load_library tsmc035_typ
+read -technology "tsmc035_typ"  { /mnt/hgfs/DCNN-Accelerator/src/common/advancedCounter.vhd }
+pre_optimize -common_logic -unused_logic -boundary -xor_comparator_optimize 
+pre_optimize -extract 
+elaborate AdvancedCounter -architecture behavioural
+set input2output 1.200000
+optimize .work.AdvancedCounter.behavioural -target tsmc035_typ -macro -delay -effort quick -hierarchy flatten 
+optimize_timing .work.AdvancedCounter.behavioural
+set novendor_constraint_file FALSE
+set sdf_names_style vhdl
+auto_write -format SDF adv_counter_syn.sdf
+auto_write -format vhdl adv_counter_syn.vhdl
+report_area adv_counter_Area_Report.rpt -cell_usage -all_leafs
+report_delay adv_counter_delay.rpt -num_paths 1 -longest_path -clock_frequency
