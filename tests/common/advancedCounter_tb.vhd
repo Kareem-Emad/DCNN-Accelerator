ca366978b9abe175da2ee4071c2871f506a72b13
rka <akregeb@gmail.com>
2019-04-11 18:01:26 +0200

    advanced counter post-synthesis tb

--- tests/common/advancedCounter_tb.vhd
+++ tests/common/advancedCounter_tb.vhd
@@ -7,9 +7,7 @@ end AdvancedCounterTB;
 
 architecture TB of AdvancedCounterTB is
 
-    constant N: integer := 4;
-
- 
+    constant N: integer := 5;
 
     signal clk         : std_logic;
     signal reset         : std_logic;
@@ -18,10 +16,11 @@ architecture TB of AdvancedCounterTB is
     signal max_val     : std_logic_vector (n-1 downto 0);
     signal max_reached : std_logic;
     signal counter_out : std_logic_vector (n-1 downto 0);
+    constant period : time := 1 ns;
 
 begin
 
-    dut : entity work.AdvancedCounter generic map(N=>N)
+    AdvancedCounter_inst : entity work.AdvancedCounter
     port map (clk               => clk,
               reset             => reset,
               enable            => en,
@@ -32,61 +31,47 @@ begin
 
     stimuli : process
     begin
-        clk <= '0';
         mode <= "00";
         en<='1';
-        max_val <= "0101";
+        max_val <= "00101";
         reset <= '1';
-        wait for 10 ps;
+        wait for period;
         reset<= '0';
 
-        for i in 0 to 4 loop
-            clk<='0';
-            wait for 10 ps;
-            clk<='1';
-            wait for 10 ps;
-        end loop;
+        wait for 5 * period;
         Assert(counter_out=max_val and max_reached='1');
 
         reset <= '1';
-        wait for 10 ps;
+        wait for period;
         reset<= '0';
         Assert(max_reached='0');
 
         mode<="01";
-        max_val <= "0000";
-        for i in 0 to 15 loop
-            clk<='0';
-            wait for 10 ps;
-            clk<='1';
-            wait for 10 ps;
-        end loop;
-        Assert(counter_out="0000" and max_reached='1');
+        max_val <= "00000";
+        wait for period * 16;
+        Assert(counter_out="00000" and max_reached='1');
        
 
         mode<="10";
-        max_val <= "1000";
-        for i in 0 to 3 loop
-            clk<='0';
-            wait for 10 ps;
-            clk<='1';
-            wait for 10 ps;
-        end loop;
+        max_val <= "01000";
+        wait for period * 4;
         Assert(counter_out=max_val and max_reached='1');
 
 
         mode<="11";
-        max_val <= "0000";
-        for i in 0 to 3 loop
-            clk<='0';
-            wait for 10 ps;
-            clk<='1';
-            wait for 10 ps;
-        end loop;
+        max_val <= "00000";
+        wait for period * 4;
         Assert(counter_out=max_val and max_reached='1');
 
 
         wait;
     end process;
 
+    process is
+    begin
+        clk <= '0';
+        wait for period / 2;
+        clk <= '1';
+        wait for period / 2;
+    end process;
 end TB;
