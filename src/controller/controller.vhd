017737640343ba117493b1a8b4cbdffc2d3c9db7
rka <akregeb@gmail.com>
2019-04-15 03:36:12 +0200

    Initial window filter fetch-- need to handle edges.

--- src/controller/controller.vhd
+++ src/controller/controller.vhd
@@ -15,8 +15,8 @@ entity Controller is
         mem_data_in     :   in  std_logic_vector(N - 1 downto 0);
         mem_data_out    :   out std_logic_vector(N - 1 downto 0);
         mem_addr_out    :   out std_logic_vector(M - 1 downto 0);
-        mem_write_out   :   out std_logic;
-        mem_read_out    :   out std_logic;
+        mem_write_out   :   out std_logic; -- writes into memory
+        mem_read_out    :   out std_logic; -- reads from memory
         filter_data_out :   out std_logic_vector(N-1 downto 0)
     );
 end Controller;
@@ -34,24 +34,141 @@ architecture Mixed of Controller is
         end_state
     );
     signal current_state : state_type := init_filter_window;
-    signal next_state : state_type := init_image_cache;
+    signal next_state : state_type := init_filter_window;
+    signal cntr1_reset : std_logic := '0';
+    signal cntr1_enable : std_logic := '1';
+    signal cntr1_mode : std_logic := '0';
+    signal cntr1_max_val : std_logic_vector(4 downto 0) := (others => '1');
+    signal cntr1_max_reached : std_logic := 'Z';
+    signal cntr1_data : std_logic_vector(4 downto 0) := (others => 'Z');
 
+    signal fltSize_reset : std_logic := '0';
+    signal fltSize_load : std_logic := '0';
+    -- signal fltSize_data_in : std_logic_vector(N-1 downto 0) := '0';
+    signal fltSize_data_out : std_logic_vector(N-1 downto 0) := (others => '0');
+    signal fltSize_rst_data : std_logic_vector(N-1 downto 0) := (others => '0');
+
+    signal bias_reset : std_logic := '0';
+    signal bias_load : std_logic := '0';
+    signal bias_data_out : std_logic_vector(N-1 downto 0) := (others => '0');
+    signal bias_rst_data : std_logic_vector(N-1 downto 0) := (others => '0');
+
+    signal addr1_reset : std_logic := '0';
+    signal addr1_enable : std_logic := '1';
+    signal addr1_mode : std_logic := '0';
+    signal addr1_max_reached : std_logic := 'Z';
+    signal addr1_data : std_logic_vector(M-1 downto 0) := (others => 'Z');
+
+    signal fltSize_squaredp3 : std_logic_vector(4 downto 0) := (others => 'Z');
+    signal write_mem_to_fltr : std_logic := '0';
 begin
+    fltSize_squaredp3 <= "11100" when fltSize_data_out = X"0005" else "01100";
+    filter_data_out <= mem_data_in when write_mem_to_fltr = '1' else (others => '0');
+    cntr1_inst : entity dcnn.Counter
+    port map (
+        clk => clk,
+        reset => cntr1_reset,
+        enable => cntr1_enable,
+        mode_in => cntr1_mode,
+        max_val_in => cntr1_max_val,
+        max_reached_out => cntr1_max_reached,
+        counter_out => cntr1_data
+    );
+    
+    cntr_mem_addr : entity dcnn.Counter
+    generic map (
+        N => 16
+    ) 
+    port map (
+        clk => clk,
+        reset => addr1_reset,
+        enable => addr1_enable,
+        mode_in => addr1_mode,
+        max_val_in => (others => '1'),
+        max_reached_out => addr1_max_reached,
+        counter_out => addr1_data
+    );
+
+    reg_filter_height : entity dcnn.Reg
+    port map (
+        clk => clk,
+        reset => fltSize_reset,
+        load => fltSize_load,
+        d => mem_data_in,
+        q => fltSize_data_out,
+        rst_data => fltSize_rst_data
+    );
+
+    reg_bias1 : entity dcnn.Reg
+    port map (
+        clk => clk,
+        reset => bias_reset,
+        load => bias_load,
+        d => mem_data_in,
+        q => bias_data_out,
+        rst_data => bias_rst_data
+    );
+
     -- This process computes the next state given the current state and the inputs.
-    comp_ns : process(current_state)
+    comp_ns : process(current_state, cntr1_data, fltSize_squaredp3, cntr1_max_reached)
     begin
         case current_state is
+            -- Fetch the filter window; Needs a counter.
             when init_filter_window =>
-                -- Fetch the filter window; Needs a counter.
-                -- 1: Load Filter Width
-                -- 2: Load Filter Height
-                -- 3: Load Bias
-                -- 4: If FilterWidth=3, Set Imax=9. Else Set Imax=25.
-                -- 5: Loop: for i = 0; i < Imax; i++
-                -- 6:   If Edge: Write 0. 
-                -- 7:   Else: Fetch pixel from current mem loc.
-            when init_image_cache =>
+                -- Assumes the counter starts at the right memory address.
+                case cntr1_data is
+                    when "00000" => -- 1: Load Filter Size
+                        -- Filter data not ready yet.
+                        write_mem_to_fltr <= '0';
+                        -- Prepare the counter for the future.
+                        cntr1_reset <= '0';
+                        cntr1_enable <= '1';
+                        cntr1_mode <= '0';
+                        cntr1_max_val <= "11111";
 
+                        -- Prepare the memory address counter for the future.
+                        addr1_reset <= '0';
+                        addr1_enable <= '1';
+                        addr1_mode <= '0';
+                        
+                        -- Load Filter Size from memory
+                        fltSize_reset <= '0';
+                        fltSize_load <= '1';
+                        mem_addr_out <= addr1_data;
+                        mem_write_out <= '0';
+                        mem_read_out <= '1';
+
+                        -- The next state will be the same because we change nothing.
+                        next_state <= current_state;
+                    when "00001" => -- 2: Load Bias
+                        fltSize_load <= '0';
+                        cntr1_max_val <= fltSize_squaredp3;
+
+                        -- Load bias from memory
+                        mem_addr_out <= addr1_data;
+                        mem_read_out <= '1';
+                        bias_reset <= '0';
+                        bias_load <= '1';
+                        
+                        next_state <= current_state;
+                    -- when "00010" => 
+                    when others => -- 3: Fetch to the filter window.
+                        bias_load <= '0';
+                        if cntr1_max_reached = '0' then -- 4: Loop: for i = 0; i < Imax; i++
+                            -- 5:   If Edge: Write 0. Ignored for now..
+                            -- 6:   Else: Fetch pixel from current mem loc.
+                            mem_addr_out <= addr1_data;
+                            mem_read_out <= '1';
+                            write_mem_to_fltr <= '1';
+                        else
+                            cntr1_reset <= '1';
+                            mem_read_out <= '0';
+                            write_mem_to_fltr <= '0';
+                            next_state <= end_state;
+                        end if;
+                    end case;
+            when init_image_cache =>
+                    
             when start_convolution =>
 
             when fetch_to_cache =>
@@ -64,7 +181,7 @@ begin
 
             when write_classification =>
 
-            when end_state =>
+            when others =>
                 next_state <= current_state;
             end case;
     end process;
--- tests/controller/controller_tb.vhd
+++ tests/controller/controller_tb.vhd
@@ -8,7 +8,7 @@ end ControllerTB;
 
 architecture TB of ControllerTB is
     signal clk, read_mem, write_mem : std_logic := '0';
-    signal address : std_logic_vector(7 downto 0) := (others => 'Z');
+    signal address : std_logic_vector(15 downto 0) := (others => 'Z');
     signal data_into_mem, data_outof_mem : std_logic_vector(15 downto 0) := (others => 'Z');
     signal filter_data : std_logic_vector(15 downto 0) := (others => 'Z');
     signal io_ready : std_logic := '1';
@@ -30,11 +30,24 @@ begin
             io_ready_in => io_ready,
             mem_data_in => data_outof_mem,
             mem_data_out => data_into_mem,
-            mem_addr_out => addr,
+            mem_addr_out => address,
             mem_read_out => read_mem,
             mem_write_out => write_mem,
             filter_data_out => filter_data
         );
 -- 2: Initialize memory values (maybe in the simulator?)
 -- 3: Test the initialization of the filter window.
+
+    process is
+    begin
+        wait for period * 4;
+    end process;
+
+    process is
+    begin
+        clk <= '0';
+        wait for period / 2;
+        clk <= '1';
+        wait for period / 2;
+    end process;
 end TB;
